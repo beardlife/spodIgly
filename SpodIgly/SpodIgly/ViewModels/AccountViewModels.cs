d112e3fa1b41f6c11edbed8e0bb812ca15c830a0
Kuczynski Dawid <kuczynskidaw@gmail.com>
2017-07-29 22:25:16 +0200

    added login and register view with validation

---
+++ SpodIgly/SpodIgly/ViewModels/AccountViewModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Podaj login")]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Podaj hasło")]
+        public string Password { get; set; }
+        
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage="The {0} must be at least {2} characters long", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
