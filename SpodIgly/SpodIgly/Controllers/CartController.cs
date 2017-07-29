1cf3f832761d97d7707a6ef36612d85a1c3a2e31
Kuczynski Dawid <kuczynskidaw@gmail.com>
2017-07-29 20:39:01 +0200

    added cart functionality with session manager

---
+++ SpodIgly/SpodIgly/Controllers/CartController.cs
@@ -0,0 +1,65 @@
+﻿using SpodIgly.DAL;
+using SpodIgly.Infrastructure;
+using SpodIgly.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpodIgly.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private ShoppingCartManager shoppingCartManager;
+        private ISessionManager sessionManager { get; set; }
+        private StoreContext db = new StoreContext();
+        //
+        // GET: /Cart/
+
+        public CartController()
+        {
+            this.sessionManager = new SessionManager();
+            this.shoppingCartManager = new ShoppingCartManager(sessionManager, db);
+        }
+
+        public ActionResult Index()
+        {
+            var cartItems = shoppingCartManager.GetCart();
+            var cartTotalPrice = shoppingCartManager.GetCartTotalPrice();
+
+            var cartVM = new CartViewModel() { CartItems = cartItems, TotalPrice = cartTotalPrice };
+
+            return View(cartVM);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            shoppingCartManager.AddToCart(id);
+            return RedirectToAction("Index");
+        }
+
+        public int getCartItemsCount()
+        {
+            return shoppingCartManager.GetCartItemsCount();
+        }
+
+        public ActionResult RemoveFromCart(int albumID)
+        {
+            int itemCount = shoppingCartManager.RemoveFromCart(albumID);
+            int cartItemsCount = shoppingCartManager.GetCartItemsCount();
+            decimal cartTotal = shoppingCartManager.GetCartTotalPrice();
+
+            var result = new CartRemoveViewModel
+            {
+                RemoveItemId = albumID,
+                RemovedItemCount = itemCount,
+                CartTotal = cartTotal,
+                CartItemsCount = cartItemsCount
+            };
+
+            return Json(result);
+        }
+	}
+}
