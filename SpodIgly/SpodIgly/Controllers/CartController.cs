cdaba4d7be85b3ec68f73e8f04c95cdc266ca1fd
Kuczynski Dawid <kuczynskidaw@gmail.com>
2017-08-04 14:22:54 +0200

    added order functionality

--- SpodIgly/SpodIgly/Controllers/CartController.cs
+++ SpodIgly/SpodIgly/Controllers/CartController.cs
@@ -1,11 +1,17 @@
-﻿using SpodIgly.DAL;
+﻿using SpodIgly.App_Start;
+using SpodIgly.DAL;
 using SpodIgly.Infrastructure;
 using SpodIgly.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using SpodIgly.Models;
+
 
 namespace SpodIgly.Controllers
 {
@@ -60,6 +66,72 @@ namespace SpodIgly.Controllers
             };
 
             return Json(result);
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
+        public async Task<ActionResult> Checkout()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+                var order = new Order
+                {
+                    FirstName = user.UserData.FirstName,
+                    LastName = user.UserData.LastName,
+                    Address = user.UserData.Address,
+                    CodeAndCity = user.UserData.CodeAndCity,
+                    Email = user.UserData.Email,
+                    PhoneNumber = user.UserData.PhoneNumber
+                };
+
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account", new { returnUrl = Url.Action("Checkout", "Cart") });
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Checkout(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.Identity.GetUserId();
+
+                var newOrder = shoppingCartManager.CreateOrder(order, userId);
+
+                var user = await UserManager.FindByIdAsync(userId);
+                TryUpdateModel(user.UserData);
+                await UserManager.UpdateAsync(user);
+
+                shoppingCartManager.EmptyCart();
+
+                return RedirectToAction("OrderConfirmation");
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ActionResult OrderConfirmation()
+        {
+            return View();
         }
 	}
 }
--- SpodIgly/SpodIgly/Models/Order.cs
+++ SpodIgly/SpodIgly/Models/Order.cs
@@ -10,16 +10,36 @@ namespace SpodIgly.Models
     {
         public int OrderId { get; set; }
 
-        [StringLength(150)]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić imię")]
+        [StringLength(100)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzć nazwisko")]
         [StringLength(150)]
         public string LastName { get; set; }
-        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Nie wprowadzono adresu")]
         [StringLength(150)]
-        [Required(ErrorMessage="Wprowadź kod pocztowy i miasto")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź kod pocztowy i miasto")]
+        [StringLength(50)]
         public string CodeAndCity { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić numer telefonu")]
+        [StringLength(20)]
+        [RegularExpression(@"(\+\d{2})*[\d\s-]+",
+            ErrorMessage = "Błędny format numeru telefonu.")]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź swój adres e-mail.")]
+        [EmailAddress(ErrorMessage = "Błędny format adresu e-mail.")]
         public string Email { get; set; }
+        
+        
         public string Comment { get; set; }
         public DateTime DateCreated { get; set; }
         public OrderState OrderState { get; set; }
